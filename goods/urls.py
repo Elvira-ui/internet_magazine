63bfca67c8d3bc8a4a67ef4d53aaacdb2cc5d152
Elvira-ui <elviranabiyeva11@gmail.com>
2024-12-27 08:09:29 +0400

    Simple search created

--- goods/urls.py
+++ goods/urls.py
@@ -1,9 +1,10 @@
-from django.urls import include, path
+from django.urls import path
 from goods import views
 
 app_name='goods'
 
-urlpatterns: list = [
+urlpatterns=[
+    path('search/', views.catalog, name='search'),
     path('<slug:category_slug>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
    
