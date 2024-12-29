15d0dc618faa7abe22f99ca5c1d48ad071233cc2
Elvira-ui <elviranabiyeva11@gmail.com>
2024-12-29 11:29:54 +0400

    first

--- app/settings.py
+++ app/settings.py
@@ -88,9 +88,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
+        
     }
 }
+
 
 
 # Password validation
